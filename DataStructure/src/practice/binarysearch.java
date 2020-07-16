0926971f899cac2b05393dc17692033aafadf86c
satyam <satyam@LAPTOP-1EB12NJK>
2020-07-16 19:59:20 +0530

    bubbleosrt

--- DataStructure/src/practice/binarysearch.java
+++ DataStructure/src/practice/binarysearch.java
@@ -1,10 +1,52 @@
 package practice;
 
+import java.util.Scanner;
+
 public class binarysearch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner s= new Scanner(System.in);
+		System.out.println("size of array");
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		System.out.println("enter element to be search");
+		int x=s.nextInt();
+		b.binarysearch(a,x);
+		}
+
+	void binarysearch(int a[],int x)
+		{
+			int first=0;
+			int last=a.length-1;
+			int m;
+			while(first<=last)
+			{
+				m=first+(last-first)/2;
+				if(a[m]==x)
+				{
+					System.out.println(m);
+					break;
+				}
+				if(a[m]>x)
+				{
+					last=m-1;
+				}
+				if(a[m]<x)
+				{
+					first=m+1;
+				}
+			
+			}
+		//	s.close();
+			
+			
+		}
 	}
 
-}
+
---
+++ DataStructure/src/practice/sortwithbubblesort.java
@@ -0,0 +1,36 @@
+package practice;
+
+import java.util.Scanner;
+
+public class sortwithbubblesort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s= new Scanner(System.in);
+		System.out.println("size of array");
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{int temp=0;
+			for(int j=0;j<n-i-1;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					 temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
